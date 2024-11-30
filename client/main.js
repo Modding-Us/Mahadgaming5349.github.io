73c11318583767ae0e2e714fc729f6f0d75d0339
Mahadgaming5349 <127315215+Mahadgaming5349@users.noreply.github.com>
2024-11-30 17:18:25 +0600

    main.js

--- client/main.js
+++ client/main.js
@@ -1,9 +1,9 @@
 import './style.css'
-import rocketLogo from '/rocket.png'
+import rocketLogo from '/Picsart_24-10-24_16-51-25-175.png'
 
 document.querySelector('#app').innerHTML = `
   <div>
     <img src="${rocketLogo}" class="logo" alt="Discord" />
-    <h1>Hello, World!</h1>
+    <h1>Your SUS!</h1>
   </div>
 `;
